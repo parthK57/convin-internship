3074fa32b867b88f73cc5aa7fea018dca1b88858
parthK57 <parth.kolgiri32@gmail.com>
2023-03-28 14:47:19 +0530

    sign up page

---
+++ frontend/src/components/SignUpForm.tsx
@@ -0,0 +1,83 @@
+import { useState } from "react";
+
+const SignUpForm = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+
+  const signUpUser = (e: any) => {
+    e.preventDefault();
+    // TODO: CREATE AXIOS POST REQ
+    try {
+      if (confirmPassword !== password)
+        throw new Error("Passwords do not match!");
+      else console.log(name, email, password, confirmPassword);
+    } catch (error: any) {
+      alert(error.message);
+    }
+  };
+
+  return (
+    <>
+      <form
+        autoComplete="off"
+        className="flex w-[340px] flex-col gap-1 overflow-hidden rounded-3xl bg-indigo-50 pb-5 shadow-md sm:w-[50%] md:w-[69%] lg:w-[70%] xl:w-[47%]"
+      >
+        <div className="flex h-[80px] items-center justify-center shadow-sm">
+          <div className="text-2xl">Sign Up</div>
+        </div>
+        <div className="flex flex-col gap-2 px-4">
+          <label>Name :</label>
+          <input
+            required
+            type="text"
+            onChange={(e) => setName(e.target.value)}
+            className="rounded-md px-2 py-1"
+          />
+        </div>
+        <div className="flex flex-col gap-2 px-4">
+          <label>Email :</label>
+          <input
+            required
+            type="text"
+            className="rounded-md px-2 py-1"
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+        <div className="flex flex-col gap-2 px-4">
+          <label>Password :</label>
+          <input
+            required
+            onChange={(e) => setPassword(e.target.value)}
+            type="password"
+            className="rounded-md px-2 py-1"
+          />
+        </div>
+        <div className="flex flex-col gap-2 px-4">
+          <label>Retype Password :</label>
+          <input
+            required
+            type="password"
+            onChange={(e) => setConfirmPassword(e.target.value)}
+            className="rounded-md px-2 py-1"
+          />
+        </div>
+        <div className="mt-4 flex justify-start gap-6 px-4">
+          <button
+            type="submit"
+            onClick={signUpUser}
+            className="rounded-lg bg-indigo-600 px-3 py-2 text-white transition-all ease-in-out hover:bg-green-500"
+          >
+            Submit
+          </button>
+          <button className="rounded-lg bg-[#e02d51] px-3 py-2 text-white hover:bg-[#e24363]">
+            Cancel
+          </button>
+        </div>
+      </form>
+    </>
+  );
+};
+
+export default SignUpForm;
--- frontend/src/pages/SignUp.tsx
+++ frontend/src/pages/SignUp.tsx
@@ -1,11 +1,20 @@
-import Navbar from "../components/Navbar"
+import Navbar from "../components/Navbar";
+import SignUpForm from "../components/SignUpForm";
 
 const SignUp = () => {
   return (
     <>
-    <Navbar />
+      <Navbar />
+      <div className="grid grid-cols-1 md:grid-cols-2">
+        <div className="col hidden h-[calc(100vh-70px)] items-center justify-center md:flex">
+          <div className="h-[50%] w-[50%] overflow-hidden  bg-[url('https://img.freepik.com/free-vector/freelancer-working-laptop-her-house_1150-35048.jpg?w=740&t=st=1679991235~exp=1679991835~hmac=e95508798275b0280d6c2e8e1e3604beb6d31dc7d2730b441b544543eb2cd4a3')] bg-cover bg-center bg-no-repeat"></div>
+        </div>
+        <div className="col flex h-[calc(100vh-70px)] items-center justify-center">
+          <SignUpForm />
+        </div>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default SignUp
+export default SignUp;
